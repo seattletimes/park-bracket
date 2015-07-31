edfa1cbaaf6a8f0525723c625a6d7cd8ca3b2aaf
Thomas Wilburn <news@thomaswilburn.net>
2015-07-31 14:56:59 -0700

    Faster cookies, less indentation

--- src/js/evercookie.js
+++ src/js/evercookie.js
@@ -20,33 +20,27 @@ var tab = function(key, value) {
 };
 tab.clear = () => window.name = "";
 
-var parseCookie = function() {
-  var result = {};
-  var pairs = document.cookie.split(";");
-  pairs.forEach(function(pair) {
-    var split = pair.split("=");
-    var value = decodeURI(split[1]);
-    try {
-      value = JSON.parse(value);
-    } catch(_) { }
-    result[split[0].trim()] = value;
-  });
-  return result;
-};
+var cookies = {};
+var pairs = document.cookie.split(";");
+pairs.forEach(function(pair) {
+  var split = pair.split("=");
+  var value = decodeURI(split[1]);
+  try {
+    value = JSON.parse(value);
+  } catch(_) { }
+  cookies[split[0].trim()] = value;
+});
 
 var cookie = function(key, value) {
-  return new Promise(function(ok, fail) {
-    if (value) {
-      value = encodeURI(JSON.stringify(value));
-      document.cookie = `${key}=${value}; path=/; expires=Fri, 31 Dec 9999 23:59:59 GMT`;
-      ok();
-    } else {
-      var cookies = parseCookie();
-      ok(cookies[key]);
-    }
-  });
+  if (value) {
+    value = encodeURI(JSON.stringify(value));
+    document.cookie = `${key}=${value}; path=/; expires=Fri, 31 Dec 9999 23:59:59 GMT`;
+    cookies[key] = value;
+    return Promise.resolve();
+  } else {
+    return Promise.resolve(cookies[key]);
+  }
 };
-
 cookie.clear = (key) => document.cookie = `${key}=;path=/;expires=Thu, 1 Jan 1970 00:00:00 GMT`;
 
 var Database = require("./idb");
@@ -59,26 +53,24 @@ var db = new Database("evercookie", 1, function() {
 
 var idb = function(key, value) {
   if (value) {
-    return db.ready.then(() => db.put("cookies", { key: key, value: value}));
+    return db.ready.then(_ => db.put("cookies", { key: key, value: value}));
   } else {
-    return db.ready.then(() => db.get("cookies", key)).then(result => result ? result.value : null);
+    return db.ready.then(_ => db.get("cookies", key)).then(result => result ? result.value : null);
   }
 };
 idb.clear = () => db.ready.then(() => db.clear("cookies"));
 
 var localS = function(key, value) {
-  return new Promise(function(ok, fail) {
-    if (value) {
-      window.localStorage.setItem(key, encodeURI(JSON.stringify(value)));
-      ok();
-    } else {
-      var result = decodeURI(window.localStorage.getItem(key));
-      try {
-        result = JSON.parse(result);
-      } catch(_) {}
-      ok(result);
-    }
-  });
+  if (value) {
+    window.localStorage.setItem(key, encodeURI(JSON.stringify(value)));
+    return Promise.resolve();
+  } else {
+    var result = decodeURI(window.localStorage.getItem(key));
+    try {
+      result = JSON.parse(result);
+    } catch(_) {}
+    return Promise.resolve(result);
+  }
 };
 localS.clear = window.localStorage.clear.bind(window.localStorage);
 
@@ -95,7 +87,6 @@ var facade = {
         promises[i].then(check, skip);
       };
       var check = function(value) {
-        console.log("check", i);
         if (value) return ok(value);
         skip();
       };
