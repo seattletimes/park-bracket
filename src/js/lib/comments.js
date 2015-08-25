7f349e7b02b1b71091abcf6e05c35281e4b218ea
Thomas Wilburn <news@thomaswilburn.net>
2015-08-25 15:11:07 -0700

    Added comments
    This code is horrible, we need a better solution by the next big
    project.

--- src/js/lib/comments.js
+++ src/js/lib/comments.js
@@ -27,59 +27,62 @@ var async = [
 
 var head = document.querySelector("head");
 var config = document.querySelector("script[type='livefyre-config']");
-if (!config) return;
-config = config.innerHTML;
-config = JSON.parse(config);
+if (config) {
 
-css.forEach(function(url) {
-  var link = document.createElement("link");
-  link.type = "text/css";
-  link.rel = "stylesheet";
-  link.href = url;
-  head.appendChild(link);
-});
+  config = config.innerHTML;
+  config = JSON.parse(config);
 
-var scriptIndex = -1;
-
-var configure = function() {
-  fyre.conv.load({
-    strings: {
-      postAsButton: "Post comment",
-      postReplyAsButton: "Post comment",
-      signIn: "You must be logged in to leave a comment. Log in or create an account.",
-      listenerCountPlural: "People Viewing",
-      listenerCount: "Person Viewing",
-      moderator: "Seattle Times staff",
-      signOut: "Log out",
-      backToComments: "View all comments"
-    },
-    authDelegate : authDelegate,
-    network: "seattletimes.fyre.co"
-  },
-  [{
-    app: "main",
-    siteId: "316317",
-    articleId: config.articleId,
-    el: "livefyre-comments",
-    checksum: config.checksum,
-    collectionMeta: config.collectionMeta
-  }], function (widget) {
-    callback();
+  css.forEach(function(url) {
+    var link = document.createElement("link");
+    link.type = "text/css";
+    link.rel = "stylesheet";
+    link.href = url;
+    head.appendChild(link);
   });
-};
 
-var asyncScripts = function() {
-  //console.log(this, scriptIndex);
-  scriptIndex++;
-  var url = async[scriptIndex];
-  if (!url) {
-    return configure();
-  }
-  var script = document.createElement("script");
-  script.src = url;
-  script.onload = asyncScripts;
-  head.appendChild(script);
-};
+  var scriptIndex = -1;
 
-//load comments after 5 seconds
-setTimeout(asyncScripts, 5 * 1000);
+  var configure = function() {
+    fyre.conv.load({
+      strings: {
+        postAsButton: "Post comment",
+        postReplyAsButton: "Post comment",
+        signIn: "You must be logged in to leave a comment. Log in or create an account.",
+        listenerCountPlural: "People Viewing",
+        listenerCount: "Person Viewing",
+        moderator: "Seattle Times staff",
+        signOut: "Log out",
+        backToComments: "View all comments"
+      },
+      authDelegate : authDelegate,
+      network: "seattletimes.fyre.co"
+    },
+    [{
+      app: "main",
+      siteId: "316317",
+      articleId: config.articleId,
+      el: "livefyre-comments",
+      checksum: config.checksum,
+      collectionMeta: config.collectionMeta
+    }], function (widget) {
+      callback();
+    });
+  };
+
+  var asyncScripts = function() {
+    //console.log(this, scriptIndex);
+    scriptIndex++;
+    var url = async[scriptIndex];
+    if (!url) {
+      return configure();
+    }
+    var script = document.createElement("script");
+    script.src = url;
+    script.onload = asyncScripts;
+    head.appendChild(script);
+  };
+
+  //load comments after 5 seconds
+  setTimeout(asyncScripts, 5 * 1000);
+  
+}
